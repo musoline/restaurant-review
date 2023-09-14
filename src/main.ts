01864427bab60248e26660cfa23a9402b321c6e2
musoline <yruashvilinika@yahoo.com>
2023-09-14 21:50:49 +0400

    add seeders and Restaurant/Review entities, setup API partially

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,12 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { SeederService } from './seeder/seeder.service';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const seederService = app.get(SeederService);
+  await seederService.seedData();
 
   app.enableCors({
     origin: 'http://localhost:4200',
