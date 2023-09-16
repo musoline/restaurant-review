12354c6a1e2e2108e8c3dc9656f33ef2b16dd372
musoline <yruashvilinika@yahoo.com>
2023-09-17 03:56:20 +0400

    add swagger

--- src/main.ts
+++ src/main.ts
@@ -1,16 +1,27 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { SeederService } from './seeder/seeder.service';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  const config = new DocumentBuilder()
+    .setTitle('Restaurant Review')
+    .setDescription('The restaurant API description')
+    .setVersion('1.0')
+    .addTag('restaurants')
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('swagger-api', app, document);
 
   const seederService = app.get(SeederService);
   await seederService.seedData();
 
   app.enableCors({
-    // origin: 'http://localhost:4200',
-    // credentials: true,
+    origin: 'http://localhost:4200',
+    credentials: true,
   });
   await app.listen(3000);
 }
